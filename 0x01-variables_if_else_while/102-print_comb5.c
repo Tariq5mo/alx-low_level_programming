0f0908bb595322371d9e204ab65e5f717a9eb57d
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-09-11 12:31:51 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1, n2, n3, n4, num1, num2;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			for (n3 = 0; n3 <= 9; n3++)
+			{
+				for (n4 = 0; n4 <= 9; n4++)
+				{
+					num1 = n1 * 10 + n2;
+					num2 = n3 * 10 + n4;
+					if (num2 <= num1)
+						continue;
+					putchar(n1 + '0');
+					putchar(n2 + '0');
+					putchar(' ');
+					putchar(n3 + '0');
+					putchar(n4 + '0');
+					if (!(num1 == 98 && num2 == 99))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
