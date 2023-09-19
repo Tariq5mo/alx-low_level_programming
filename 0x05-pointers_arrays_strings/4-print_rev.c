0b10890970033b0909ef05c1b5c64f941d30c91e
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-09-19 00:05:23 -0700

    F

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,34 @@
 #include "main.h"
 
 /**
+ * _strlen - reverse and print
+ *
+ * @s: the array which reverse and print
+ * Return: i the lenght
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+
+		return (i);
+	}
+}
+/**
  * print_rev - reverse and print
  *
  * @s: the array which reverse and print
  */
-int _strlen(char *s)
-{
-        int i;
-        i = 0;
-        while (*(s + i) != '\0')
-        {
-                i++;
-        }
-        return (i);
-}
 void print_rev(char *s)
 {
 	int i;
 
 	i = _strlen(s);
-	for (; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(*(s + i));
 	}
