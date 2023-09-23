96a54b1c005268462c77383abb45b55a5f9f8829
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-09-22 22:52:31 -0700

    7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 #include <stdio.h>
 int _putchar(char c);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void print_rev(char *s);
