62e1b32838112877c1893dc8cc7f99f0b1be1cc8
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-09-26 11:03:37 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - search for char
+ *
+ * @s: the string
+ * @c: the char
+ * Return: array or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int l;
+
+	for (l = 0; *(s + l) != '\0'; l++)
+	{
+		if (*(s + l) == c)
+		{
+			return (s + l);
+		}
+	}
+	return (NULL);
+}
