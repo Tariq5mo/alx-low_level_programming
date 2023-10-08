78e59ba417a8e1a3071dc8edf3b0f57d146ba6a1
Tariq5mo <tariqomer756@gmail.com>
2023-10-08 09:02:14 +0300

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory
+ *
+ * @b: the size
+ * Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(sizeof(unsigned int) * b);
+	if (p == NULL)
+		exit(98);
+}
