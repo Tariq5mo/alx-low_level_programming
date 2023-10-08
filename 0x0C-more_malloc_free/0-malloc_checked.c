005b40da9b51ddbfc5c8ca647c6457eda912a3d9
Tariq5mo <tariqomer756@gmail.com>
2023-10-08 09:24:42 +0300

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,9 +10,9 @@ void *malloc_checked(unsigned int b)
 {
 	unsigned int *p;
 
-	p = malloc(sizeof(unsigned int) * b);
+	p = malloc(b);
 	if (p == NULL)
 		exit(98);
 	else
-		exit(0);
+		return (p);
 }
