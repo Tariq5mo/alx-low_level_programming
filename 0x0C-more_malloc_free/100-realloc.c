c21187034ea41b39fe1352efe7a0e48b272bb2cc
Tariq5mo <tariqomer756@gmail.com>
2023-10-09 01:22:56 +0300

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == 0 && ptr != NULL)
-		return (NULL);
+	{
+		free(ptr);
+		return (NULL);	
+	}
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
