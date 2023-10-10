270b128b3ee8ff0cebf3ccc28ec2f5a646b1dfe8
Tariq5mo <tariqomer756@gmail.com>
2023-10-11 02:28:52 +0300

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - for poppy
+ *
+ * @name: it's name
+ * @age:  it's age
+ * @owner: who is the owner
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
