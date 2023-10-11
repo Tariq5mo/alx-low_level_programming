ccbe92c9b931453a393dbeb3e09064202ac1d3a2
Tariq5mo <tariqomer756@gmail.com>
2023-10-11 05:14:30 +0300

    4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - for poppy
  *
@@ -17,4 +18,5 @@ typedef struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
