8dc9b44e1c8f6ed664d2e0ea818cea66edc8f90a
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-10-22 01:10:19 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n < 0)
-		exit (1);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i < n - 1)
 			printf(", ");
 	}
+	printf("\n");
 }
