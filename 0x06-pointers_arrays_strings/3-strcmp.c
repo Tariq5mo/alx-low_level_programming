53abb76b11564c2fba273afc8fb0f4debdb1f7b5
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-10-23 08:16:53 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,8 +15,8 @@ int _strcmp(char *s1, char *s2)
 		;
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
-	else if (s1[i] >= s2[i])
-		return (1);
+	else if (s1[i] > s2[i])
+		return (15);
 	else
-		return (-1);
+		return (-15);
 }
