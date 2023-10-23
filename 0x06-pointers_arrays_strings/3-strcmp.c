c822e1e5f73f9b8e0140ec050e44524f5be320c6
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-10-23 02:52:52 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare between two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 are equals, 1 s1 > s2, -1 s1 < s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		;
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	if (s1[i] == '\0')
+		return (-15);
+	else
+		return (15);
+}
