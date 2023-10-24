9b25ddfa7b9fb9f013492bb4c3494638eed3e589
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-10-24 04:47:52 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - Convert to leet
+ *
+ * @s: The string
+ * Return: The modified string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char *sA = "AEOTL";
+	char *sa = "aeotl";
+	char *sn = "43071";
+
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == sA[i] || s[j] == sa[i])
+				s[j] = sn[i];
+		}
+	}
+	return (s);
+}
