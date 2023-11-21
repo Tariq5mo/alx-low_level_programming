fa51f97c048dedc40ba863f8fd15f6a2e79ca6a0
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-11-21 10:07:47 +0100

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,6 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	printf("%d\t%d\n", _strlen(s1), _strlen(s2));
 	s = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 	if (s == NULL)
 		return (NULL);
