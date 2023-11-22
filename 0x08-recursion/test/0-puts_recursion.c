a7f5794a92674cea14256fadd774b4031d954588
Tariq5mo <tariqomer756@gmail.com@gmail.com>
2023-11-22 20:12:02 +0100

    Fix

--- 0x08-recursion/test/0-puts_recursion.c
+++ 0x08-recursion/test/0-puts_recursion.c
@@ -20,13 +20,3 @@ void _puts_recursion(char *s)
 	j++;
 	_puts_recursion(s + j);/*Go to next char*/
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	_puts_recursion("Puts with recursion");
-	return (0);
-}
