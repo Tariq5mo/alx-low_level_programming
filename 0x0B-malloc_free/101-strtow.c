9d0c0e2daff14b6a016bd4789413c2d51203f4aa
Tariq5mo <tariqomer756@gmail.com>
2024-01-17 18:59:21 -0800

    Fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * strtow - spilt string to separate words
- *
- * @str: a string
- * Return: the array of words
-=======
  * strtow - splits a string into words
  *
  * @str: The string
  * Return: Array of pointers
->>>>>>> 5b7f6c9bb422f4240d53c687aa7bb7a0df6bb340
  */
 char **strtow(char *str)
 {
@@ -36,26 +29,14 @@ char **strtow(char *str)
 		while (str[i] == ' ' || str[i] == '\t')
 			i++;
 	}
-<<<<<<< HEAD
-	s = malloc(sizeof(char *) * wo);
+	if (i >  0 && wo == 0)
+		return (NULL);
+	s = malloc((sizeof(char *) * wo) + 1);/*Array of pointers*/
 	if (s == NULL || *str == '\0' || str == NULL)
 		return (NULL);
-	for (i = 0; i < wo; i++)
-	{
-		if (i == wo - 1)
-			s[i] = malloc(sizeof(char) * (cw[i] + 2));
-		else
-			s[i] = malloc(sizeof(char) * (cw[i] + 1));
-=======
-	if (i >  0 && w == 0)
-		return (NULL);
-	s = malloc((sizeof(char *) * w) + 1);/*Array of pointers*/
-	if (s == NULL || *str == '\0' || str == NULL)
-		return (NULL);
-	for (i = 0; i < w; i++)/*Reseve memory for characters*/
+	for (i = 0; i < wo; i++)/*Reseve memory for characters*/
 	{
 		s[i] = malloc(sizeof(char) * (cw[i] + 2));
->>>>>>> 5b7f6c9bb422f4240d53c687aa7bb7a0df6bb340
 		if (s[i] == NULL)
 		{
 			for (j = i - 1; j >= 0; j--)
@@ -64,12 +45,8 @@ char **strtow(char *str)
 			return (NULL);
 		}
 	}
-<<<<<<< HEAD
-	for (i = 0, j = 0; str[i] != '\0';)
-=======
 	s[i] = NULL;
 	for (i = 0, j = 0; str[i] != '\0';)/*Fill blanks*/
->>>>>>> 5b7f6c9bb422f4240d53c687aa7bb7a0df6bb340
 	{
 		
 		while (str[i] != ' ' && str[i] !='\t' && str[i] != '\0')
@@ -86,9 +63,6 @@ char **strtow(char *str)
 		while (str[i] == ' ' || str[i] =='\t')
 			i++;
 	}
-<<<<<<< HEAD
 	s[j - 1][nu] = 0;
-=======
->>>>>>> 5b7f6c9bb422f4240d53c687aa7bb7a0df6bb340
 	return (s);
 }
