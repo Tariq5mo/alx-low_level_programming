04ba6a2e484bcee7ef7ff570b94860a9dcdc8ffc
Talal4024 <Talal4024@gmail.com>
2024-03-21 10:48:14 +0000

    Add task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 
-	for(i = 0; h; h = h->next)
+	for (i = 0; h; h = h->next)
 	{
 		printf("%d\n", h->n);/*print the node's element*/
 		i++;
