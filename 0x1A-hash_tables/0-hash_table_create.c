089fb6473deaa42b36b81de45a4fe2afa3096764
Talal4024 <Talal4024@gmail.com>
2024-04-26 15:06:21 +0000

    Add task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *p;
+	unsigned long int i;
 
 	p = malloc(sizeof(hash_table_t));
 	if (!p)
 		return (NULL);
-	p->array = malloc(size);
+	p->size = size;
+	p->array = malloc(size * sizeof(hash_node_t *));
 	if (!(p->array))
+	{
+		free(p);
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		p->array[i] = NULL;
 	return (p);
 }
