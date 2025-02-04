8042c7ca43eca6ff986793a5bd8f14b09ed6131d
Tariq5mo <tariqomer756@gmail.com>
2025-02-04 15:25:28 +0200

    Fix task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -20,15 +20,6 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (*current_p >= value || current_p - array >= size_int)
 		{
-			if (current_p - array == 0)
-			{
-				if (*current_p == value)
-					return (current_p - array);
-					printf("Value found between indexes [%ld] and [%ld]\n",
-					       (size_t)(current_p - array),
-					       (size_t)(current_p - array));
-					return (-1);
-			}
 			prev_p = current_p - step;
 			printf("Value found between indexes [%ld] and [%ld]\n",
 			       (size_t)(prev_p - array), (size_t)(current_p - array));
